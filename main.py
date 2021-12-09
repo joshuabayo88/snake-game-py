b5615c91d1e527e799b4501b351ad283aa954305
joshuabayo88 <joshuabayo88@gmail.com>
2021-12-09 10:38:58 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+screen = Screen()
+screen.setup(width=600, height=600)
+# screen.bgpic("screenshot.png")
+screen.bgcolor("black")
+screen.title("Snake Game")
+print(screen)
+
+
+screen.exitonclick()
